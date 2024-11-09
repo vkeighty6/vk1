45c3d7d058fe2eb5caff93a6bc94139d1e7db54b
vkeighty6 <118623383+vkeighty6@users.noreply.github.com>
2024-11-10 01:01:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,21 +21,15 @@ if tabs == "Proof of Identity":
         country = st.selectbox("Select the country",
                                ["Select", "United States", "Canada", "United Kingdom", "Australia", "Other"])
 
-        # Add FULL NAME as Identification Document text field
-        full_name = st.text_input("Full Name as in Identification Document", placeholder="Enter your full name")
-
         # Check if country is selected before showing it
         if country != "Select":
             st.write("Country of Document Issuance: ", country)
 
-        if full_name:
-            st.write("Full Name: ", full_name)
-
         if st.button("Next", key="next_step_1"):
-            if country != "Select" and full_name:
+            if country != "Select":
                 st.session_state['identity_step'] = 2  # Move to Step 2: Select Document Type
             else:
-                st.error("Please select a country and provide your full name before proceeding.")
+                st.error("Please select a country before proceeding.")
 
     # Step 2: Select the Type of Document
     if st.session_state['identity_step'] == 2:
